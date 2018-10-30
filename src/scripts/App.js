c3f028285067a277c7868d0c66288d3d7793e668
Antti Matikainen <antti.matikainen@sievo.com>
2018-10-30 18:24:37 +0200

    setup with react

---
+++ src/scripts/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import "./App.css";
+// import GoldenLayoutWrapper from "./GoldenLayoutWrapper";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+        {/* <GoldenLayoutWrapper /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,8 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
